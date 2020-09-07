996bb5d08f3e41e1255690b1ea8c4629c3de7a6c
Riko Nagatama <riko@traveloka.com>
2020-09-07 14:47:17 +0700

    Add an endpoint for reactor-netty

--- src/main/kotlin/com/traveloka/demo/DemoApplication.kt
+++ src/main/kotlin/com/traveloka/demo/DemoApplication.kt
@@ -2,6 +2,7 @@ package com.traveloka.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -9,6 +10,8 @@ import org.springframework.web.client.RestTemplate
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
+import reactor.netty.http.client.HttpClient
+import reactor.netty.tcp.TcpClient
 
 @SpringBootApplication
 class DemoApplication
@@ -23,10 +26,20 @@ class DemoController(webClientBuilder: WebClient.Builder) {
 	private val webClient = webClientBuilder.build()
 
 	@GetMapping("/")
-	fun demo(@RequestParam delay: Long): Mono<String> {
+	fun weclient(@RequestParam delay: Long): Mono<String> {
 		return webClient.get()
 			.uri("http://localhost:8080/delay?delay=$delay")
 			.retrieve()
 			.bodyToMono(String::class.java)
+	}
+
+	@GetMapping("/reactorNetty")
+	fun reactorNetty(@RequestParam delay: Long): Mono<String> {
+		return HttpClient.create()
+			.get()
+			.uri("http://localhost:8080/delay?delay=$delay")
+			.responseContent()
+			.aggregate()
+			.asString()
 	}
 }
